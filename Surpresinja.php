7a7321162c2cad22c67691f995c22461502063bc
Isabella Heinz <isabella.heinz12@gmail.com>
2025-11-07 10:46:56 -0300

    Inicio do Surpresinja

---
+++ Surpresinja.php
@@ -0,0 +1,10 @@
+<?php
+
+$resetar = "\033[0m";
+$azul = "\033[34m";
+$fundoAzul = "\033[44m";
+$vermelho = "\033[31m";
+$emojiGift = "\u{1F381}";
+
+
+print $emojiGift . $fundoAzul . "Bem Vindo ao Surpresinja!" . $resetar . $emojiGift;
